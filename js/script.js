6e12cf72a468cfe156916ba1efda60f609903a28
SebastianoGuzzon <sebyguzz@icloud.com>
2023-03-30 15:57:13 +0200

    Other Commit

--- js/script.js
+++ js/script.js
@@ -1,108 +1,64 @@
-let arr_bomber = [];
-  let nbr_bomber = 16;
-  let start_play = 1;
-  let difficulty = 100;
-  let y = 0;
-  let points = 0;
+const gridContainer = document.getElementById('container');
 
-  // Livelli di Difficoltà
-  do {
-    difficulty = prompt('Difficoltà:  1  |  2  |  3 ');
-} while ((difficulty != '1') && (difficulty != '2') && (difficulty != '3'))
+const easyBtn = document.getElementById("btnEasy");
+const mediumBtn = document.getElementById("btnMedium");
+const hardBtn = document.getElementById("btnHard");
 
-switch (difficulty) {
-    
-    case '1':
-        difficulty = 100;
-    break;
+easyBtn.addEventListener('click', 
+    function() {
+        gridContainer.innerHTML = "";
 
-    case '2':
-        difficulty = 80;
-    break;
+        gridDifficulty(100, "squareEasy")
+
+        squareClickAdd('[class^="square"]', 'active')
         
-    case '3':
-        difficulty = 50;
-    break;
-}
-
-while (y < nbr_bomber) {
-    
-    pos_bomber = getRndInteger(start_play, difficulty);
-
-    let check_pos_bomb = contains(arr_bomber, pos_bomber)
-
-    if (check_pos_bomb == false) {
-    arr_bomber.push(pos_bomber);
-    y++;
     }
-}
+);
 
-console.log(arr_bomber);
+mediumBtn.addEventListener('click', 
+    function() {
+        gridContainer.innerHTML = "";
 
-creaCampo(difficulty);
-document.getElementById('campo').addEventListener('click',
-    function(e) {
+        gridDifficulty(81, "squareMedium")    
 
-        let nbr_play = difficulty - nbr_bomber;
-        let nbr_player = parseInt(e.target.dataset.cella);
-        let element = document.querySelectorAll('[data-cella="' + nbr_player + '"]');
-        let check_bomb_player = contains(arr_bomber, nbr_player);
+        squareClickAdd('[class^="square"]', 'active')
         
-        do {
-
-            if (points == nbr_play){
-                alert('Hai evitato tutte le bombe! Hai vinto!');
-                document.getElementById("numero_giocate").innerHTML = "Hai totalizzato: " + points + " punti";
-                document.getElementById("array_bombe").innerHTML = "Le bombe erano in posizione " + arr_bomber;
-                return;
-            }
-
-            if (check_bomb_player == true){
-                element[0].classList.add('red');
-                
-                document.getElementById("numero_giocate").innerHTML = "Hai totalizzato: " + points + " punti";
-                document.getElementById("array_bombe").innerHTML = "Le bombe erano nelle celle: " + arr_bomber;
-                return;
-            }    
-            
-            if(element[0].classList.contains('green')) {
-                return;
-            }
-
-            if (check_bomb_player == false) {
-                points += 1;
-                element[0].classList.add('green');
-                return;
-            }
-        } while(points <= nbr_play)
     }
     
-)
+);
 
-function creaCampo(celle){
+hardBtn.addEventListener('click', 
+    function() {
+        gridContainer.innerHTML = "";
 
-    for(let i = 1; i <= celle; i++){
-        let cella = `
-            <div data-cella="${i}" id="cella">${i}</div>
-        `;
-        let templateCella = document.createElement('DIV');
-        templateCella.classList.add('square');
-        templateCella.innerHTML = cella;
-        document.getElementById('campo').appendChild(templateCella);
+        gridDifficulty(49, "squareHard")
+
+        squareClickAdd('[class^="square"]', 'active')
+    }
+);
+function squareGenerator(x, y) {
+    let gridSquare = document.createElement(x);
+    gridSquare.classList.add(y);
+    return gridSquare
+}
+
+function gridDifficulty(x, y) {
+    for (let i = 0; i < x; i++) {
+        let newElem = squareGenerator("div", y);
+        gridContainer.appendChild(newElem);
     }
 }
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
-function contains(a, obj) {
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] === obj) {
-            return true;
+function squareClickAdd(x, y) {
+    let squareClick = document.querySelectorAll(x);
+        console.log(squareClick);
+
+        for (let i = 0; i < squareClick.length; i++) {
+            squareClick[i].addEventListener ('click', 
+                function() {
+                    squareClick[i].classList.add(y);
+                    squareClick[i].innerText = i + 1;
+                }
+            );
         }
-    }
-    return false;
 }
-
-
-
